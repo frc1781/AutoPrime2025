682a84890aa914945196b16bb165f56babe82263
BryceH127 <bkharris5@cps.edu>
2026-01-14 15:05:30 -0600

    Added Commands to Auto

--- src/main/java/frc/robot/subsystems/swervedrive/Vision.java
+++ src/main/java/frc/robot/subsystems/swervedrive/Vision.java
@@ -362,7 +362,7 @@ public class Vision
     RIGHT_CAM("RightApriltag",
             new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(0), Units.degreesToRadians(0)),
             new Translation3d(Units.inchesToMeters(9.5), Units.inchesToMeters(-9), Units.inchesToMeters(10.5)),
-            VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1)),
+            VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1));
 
     // FRONT_CAM("FrontApriltag",
     //           new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(0), Units.degreesToRadians(90)),
