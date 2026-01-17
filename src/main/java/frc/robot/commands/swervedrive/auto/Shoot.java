8d775081e805d3b26e4ccfb2818b570b2276cf03
Aditya Singharoy <asingharoy@cps.edu>
2026-01-17 14:17:56 -0600

    Put timers into shoot.java

--- src/main/java/frc/robot/commands/swervedrive/auto/Shoot.java
+++ src/main/java/frc/robot/commands/swervedrive/auto/Shoot.java
@@ -1,21 +1,25 @@
 package frc.robot.commands.swervedrive.auto;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Lights;
 import frc.robot.subsystems.Lights.Colors;
 import frc.robot.subsystems.Lights.Patterns;
 
 public class Shoot extends Command {
-    
+    Timer timer;
     Lights lights;
 
     public Shoot(Lights lights) {
         this.lights = lights;
+        timer = new Timer();
     }
 
     @Override
     public void initialize() {
         System.out.println(getName() + "started");
+        timer.reset();
+        timer.start();
     }
 
     @Override
@@ -25,7 +29,7 @@ public class Shoot extends Command {
 
     @Override
     public boolean isFinished() {
-        return true;
+        return timer.get() > 5;
     }
 
     @Override
@@ -35,5 +39,7 @@ public class Shoot extends Command {
         } else {
             System.out.println(getName() + "ended");
         }
+        timer.stop();
+        timer.reset();
     }
 }
